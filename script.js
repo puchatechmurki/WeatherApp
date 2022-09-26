7a304737f8c45f7c8c5691c74731c665f28afb9e
puchatechmurki <66203520+puchatechmurki@users.noreply.github.com>
2022-09-26 16:15:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const cityName = document.querySelector('.city-name')
+const warning = document.querySelector('.warning')
+const photo = document.querySelector('.photo')
+const weather = document.querySelector('.weather')
+const temperature = document.querySelector('.temperature')
+const humidity = document.querySelector('.humidity')
+
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?lat='
+
+const API_KEY = '&appid=e791d6c2cb9f09f00f34776a1f48c196'
+const API_UNITS = '&units=metric'
+
+const getWeather = () => {
+    const city = input.value ||'London'
+    const URL = API_LINK + city + API_KEY + API_UNITS
+}
+
+axios.get(URL).then(res => {
+    const temp = res.data.main.temp
+    const hum = res.data.main.humidity
+
+    cityName.textContent = res.data.name
+
+    temperature.textContent = Math.floor(temp) + 'C'
+    humidity.textContent = hum + '%'
+})
+
+getWeather()
+
+
+

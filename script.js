6d5b9a834515afdd7042b0cd496434f816eac5a5
puchatechmurki <66203520+puchatechmurki@users.noreply.github.com>
2022-09-26 17:34:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,19 +16,34 @@ const getWeather = () => {
     const city = input.value || 'Toronto'
     const URL = API_LINK + city + API_KEY + API_UNITS
 
-
 axios.get(URL).then(res => {
     console.log(res.data)
     const temp = res.data.main.temp
     const hum = res.data.main.humidity
     const status = Object.assign({}, ...res.data.weather)
     
-
     cityName.textContent = res.data.name
     temperature.textContent = Math.floor(temp) + 'C'
     humidity.textContent = hum + '%'
     weather.textContent = status.main
-   
+
+    if(status.id >= 200 && status.id < 300){
+        photo.setAttribute('src', './img/thunderstorm.png')
+    } else if (status.id >= 300 && status.id < 400){
+        photo.setAttribute('src', './img/drizzle.png')
+    } else if (status.id >=500 && status.id < 600){
+        photo.setAttribute('src', './img/rain.png')
+    }else if (status.id >=600 && status.id < 700){
+        photo.setAttribute('src', './img/ice.png')
+    } else if (status.id >=700 && status.id < 800){
+        photo.setAttribute('src', './img/fog.png')
+    } else if (status.id === 800){
+        photo.setAttribute('src', './img/sun.png')
+    } else if (status.id >=800 && status.id < 900){
+        photo.setAttribute('src', './img/cloud.png')
+    } else {
+        photo.setAttribute('src', './img/unknow.png')
+    }
 })
 }
 
